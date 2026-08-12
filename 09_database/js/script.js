22de78df24e1de5d983866feef2f6584c8ddc6dc
hdissjsj89-jpg <hdissjsj89@gmail.com>
2026-08-12 10:21:11 +0900

    Add files via upload

--- 09_database/js/script.js
+++ 09_database/js/script.js
@@ -1,39 +1,27 @@
-<script>
+  const scriptURL =
+    "https://script.google.com/macros/s/AKfycbwYrhSNcHpl_ybxHcoM-C17wNz7N6HhO1s2CXllmpWET_ToSe7X8KUESap8DiLw0TU/exec";
 
-const scriptURL =
+  const form = document.forms["submit-to-google-sheet"];
+  const msg = document.getElementById("msg");
 
-"https://script.google.com/macros/s/AKfycbwYrhSNcHpl_ybxHcoM-C17wNz7N6HhO1s2CXllmpWET_ToSe7X8KUESap8DiLw0TU/exec"; // 자신의 app스크립트 주소 링크 넣는 위치
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
 
-const form = document.forms["submit-to-google-sheet"];
+    fetch(scriptURL, {
+      method: "POST",
+      body: new FormData(form),
+    })
+      .then(function (response) {
+        msg.innerHTML = "신청이 정상적으로 완료되었습니다.";
 
-const msg = document.getElementById("msg");
+        setTimeout(function () {
+          msg.innerHTML = "";
+        }, 5000);
 
-​
-
-form.addEventListener("submit", (e) => {
-
-e.preventDefault();
-
-fetch(scriptURL, { method: "POST", body: new FormData(form) })
-
-.then((response) => {
-
-msg.innerHTML = "Message sent successfully";
-
-setTimeout(function () {
-
-msg.innerHTML = "";
-
-}, 5000);
-
-form.reset();
-
-})
-
-.catch((error) => console.error("Error!", error.message));
-
-});
-
-</script>
-
-​
+        form.reset();
+      })
+      .catch(function (error) {
+        console.error("Error!", error.message);
+        msg.innerHTML = "전송 중 오류가 발생했습니다.";
+      });
+  });
